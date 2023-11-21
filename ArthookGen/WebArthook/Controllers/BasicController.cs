231873f55fe94b72368df7aa2192aaea2eede054
mry2610 <145834324+mry2610@users.noreply.github.com>
2023-11-21 12:30:38 +0100

    Creacion funcionando

---
+++ ArthookGen/WebArthook/Controllers/BasicController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NHibernate;
+using ArthookGen.Infraestructure.CP;
+using ArthookGen.Infraestructure.Repository.Arthook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebArthhook.Controllers
+{
+    public class BasicController:Controller
+    {
+        private ISession sessionInside;
+
+
+        protected SessionCPNHibernate session;
+
+        protected BasicController()
+        {
+        }
+
+        protected void SessionInitialize()
+        {
+            if (session == null)
+            {
+                sessionInside = NHibernateHelper.OpenSession();
+                session = new SessionCPNHibernate(sessionInside);
+            }
+        }
+
+
+        protected void SessionClose()
+        {
+            if (session != null && sessionInside.IsOpen)
+            {
+                sessionInside.Close();
+                sessionInside.Dispose();
+                session = null;
+            }
+        }
+    }
+}
+
+
