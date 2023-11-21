9962def592eec5f112dd678e7287d8145973c165
mry2610 <145834324+mry2610@users.noreply.github.com>
2023-11-21 08:07:01 +0100

    video uno hecho y funcionando

---
+++ ArthookGen/WebArthook/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using ArthookGen.ApplicationCore.CEN.Arthook;
+using ArthookGen.ApplicationCore.EN.Arthook;
+using ArthookGen.Infraestructure.Repository.Arthook;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebArthook.Models;
+
+namespace WebArthook.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            UsuarioRepository usuariorepo = new UsuarioRepository();
+            UsuarioCEN usuarioCEN = new UsuarioCEN(usuariorepo);
+            IList<UsuarioEN> listaUsu = usuarioCEN.ReadAll(0,-1);
+            return View(listaUsu);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
