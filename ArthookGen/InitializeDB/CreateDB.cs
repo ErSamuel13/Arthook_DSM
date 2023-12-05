8532acc6a8eadb63354fbe9f53bc2a58e0a55a0a
mry2610 <145834324+mry2610@users.noreply.github.com>
2023-12-05 18:08:24 +0100

    medio login a falta de los unique de oharia

--- ArthookGen/InitializeDB/CreateDB.cs
+++ ArthookGen/InitializeDB/CreateDB.cs
@@ -108,9 +108,9 @@ public static void InitializeData ()
                 /*creacion usuarios*/
                 int usu1 = usuariocen.New_ (p_pass: "mry", p_nombre: "maria", p_email: "mgeg2@alu.ua.es", p_nickname: "mry2610", p_tipoUsuario: ArthookGen.ApplicationCore.Enumerated.Arthook.TipoUsuarioEnum.usuario);
                 int usu2 = usuariocen.New_ (p_pass: "pep", p_nombre: "pepe", p_email: "pepeg2@alu.ua.es", p_nickname: "pepe3101", p_tipoUsuario: ArthookGen.ApplicationCore.Enumerated.Arthook.TipoUsuarioEnum.artista);
-                int usu3 = usuariocen.New_ (p_pass: "ter", p_nombre: "teresa", p_email: "tgcg2@alu.ua.es", p_nickname: "ter2610", p_tipoUsuario: ArthookGen.ApplicationCore.Enumerated.Arthook.TipoUsuarioEnum.usuario);
+                int usu3 = usuariocen.New_ (p_pass: "ter", p_nombre: "teresa", p_email: "tgcg1@alu.ua.es", p_nickname: "ter1610", p_tipoUsuario: ArthookGen.ApplicationCore.Enumerated.Arthook.TipoUsuarioEnum.usuario);
                 int usu4 = usuariocen.New_ (p_pass: "tera", p_nombre: "tereso", p_email: "tgcg2@alu.ua.es", p_nickname: "ter2610", p_tipoUsuario: ArthookGen.ApplicationCore.Enumerated.Arthook.TipoUsuarioEnum.usuario);
-                int usu5 = usuariocen.New_ (p_pass: "terre", p_nombre: "terremoto", p_email: "tgcg2@alu.ua.es", p_nickname: "ter2610", p_tipoUsuario: ArthookGen.ApplicationCore.Enumerated.Arthook.TipoUsuarioEnum.usuario);
+                int usu5 = usuariocen.New_ (p_pass: "terre", p_nombre: "terremoto", p_email: "tgcg3@alu.ua.es", p_nickname: "ter3610", p_tipoUsuario: ArthookGen.ApplicationCore.Enumerated.Arthook.TipoUsuarioEnum.usuario);
 
 
                 Console.WriteLine ("// ---------------------------------------------------------------------------------------------------------------------------------//");
@@ -129,7 +129,7 @@ public static void InitializeData ()
                 Console.WriteLine ("");
 
                 /*modificacion usuarios*/
-                usuariocen.Modify (p_Usuario_OID: usu3, p_pass: "ter", p_nombre: "teresita", p_email: "tgcg2@alu.ua.es", p_nickname: "ter2610", p_tipoUsuario: ArthookGen.ApplicationCore.Enumerated.Arthook.TipoUsuarioEnum.usuario);
+                usuariocen.Modify (p_Usuario_OID: usu3, p_pass: "ter", p_nombre: "teresita", p_email: "tgcg22@alu.ua.es", p_nickname: "ter42610", p_tipoUsuario: ArthookGen.ApplicationCore.Enumerated.Arthook.TipoUsuarioEnum.usuario);
                 IList<UsuarioEN> listaUsuariosm = usuariocen.ReadAll (0, 3);
                 for (int i = 0; i < 3; i++) {
                         Console.WriteLine (listaUsuariosm [i].Nombre);
@@ -418,52 +418,52 @@ public static void InitializeData ()
                 Console.WriteLine ("// ---------------------------------------------------------------------------------------------------------------------------------//");
                 Console.WriteLine ("");
 
-               //int valusu1 = valoracionusuariocen.New_ (p_puntuacion: 5, p_emVal: usu2, p_reVal: usu3);
-               // int valusu2 = valoracionusuariocen.New_ (p_puntuacion: 5, p_emVal: usu3, p_reVal: usu2);
-               //   int valusu3 = valoracionusuariocen.New_ (p_puntuacion: 5, p_emVal: usu2, p_reVal: usu2);
-                 
-               //  Console.WriteLine ("// ---------------------------------------------------------------------------------------------------------------------------------//");
-               //   IList<ValoracionUsuarioEN> listavalusu = valoracionusuariocen.ReadAll (0, 3);
-               //   Console.WriteLine ("// ---------------------------------------------------------------------------------------------------------------------------------//\n");
-               //   Console.WriteLine ("MOSTRAMOS LPOS LINEAS DE DEVOLUCIONES");
-               //   foreach (ValoracionUsuarioEN p in listavalusu) {
-               //        Console.WriteLine (p.Puntuacion);
-               //   }
-               //   Console.WriteLine ("");
-               //   Console.WriteLine ("// ---------------------------------------------------------------------------------------------------------------------------------//");
-               //   Console.WriteLine ("");
-               //   valoracionusuariocen.Modify (valusu1, p_puntuacion: 1);
-               //   Console.WriteLine ("");
-               //   Console.WriteLine ("// ---------------------------------------------------------------------------------------------------------------------------------//");
-               //   Console.WriteLine ("");
-               //   ValoracionUsuarioEN valusuen = valoracionusuariocen.ReadOID (valusu1);
-               //   Console.WriteLine (""); Console.WriteLine ("");
-               //   Console.WriteLine (valusuen.Puntuacion);
-               //   Console.WriteLine ("");
-               //  Console.WriteLine ("// ---------------------------------------------------------------------------------------------------------------------------------//");
-               //   Console.WriteLine ("");
-               //   valoracionusuariocen.Destroy (valusu1);
-               //   listavalusu = valoracionusuariocen.ReadAll (0, 3);
-               //   Console.WriteLine ("// ---------------------------------------------------------------------------------------------------------------------------------//\n");
-               //   Console.WriteLine ("MOSTRAMOS LPOS LINEAS DE DEVOLUCIONES");
-               //   foreach (ValoracionUsuarioEN p in listavalusu) {
-               //       Console.WriteLine (p.Puntuacion);
-               //   }
-                 
-                 
-                 
-                 
-               //   //              int publicacionId6 = publicacioncen.New_(p_tipo: ArthookGen.ApplicationCore.Enumerated.Arthook.TipoProdEnum.video, p_descripcion: "esto es una video", p_titulo: "Video2", p_tipoPublicacion: ArthookGen.ApplicationCore.Enumerated.Arthook.TipoPublicacionEnum.Publicacion, p_usuarioPublicacion: usu3);
-               //   //              usuariocen.Modify(p_Usuario_OID: usu3, p_pass: "ter", p_nombre: "teresita", p_email: "tgcg2@alu.ua.es", p_nickname: "ter2610", p_tipoUsuario: ArthookGen.ApplicationCore.Enumerated.Arthook.TipoUsuarioEnum.usuario);
-               //   //              usuariocen.Destroy(usu1);
-                 
-                  
-               //   Console.WriteLine("");
-               //   Console.WriteLine("// ---------------------------------------------------------------------------------------------------------------------------------//");
-               //   Console.WriteLine("// ---------------------------------------------------------------------------------------------------------------------------------//");
-               //   Console.WriteLine("// ---------------------------------------------------------------------------------------------------------------------------------//");
-               //   Console.WriteLine("");
-                 
+                //int valusu1 = valoracionusuariocen.New_ (p_puntuacion: 5, p_emVal: usu2, p_reVal: usu3);
+                // int valusu2 = valoracionusuariocen.New_ (p_puntuacion: 5, p_emVal: usu3, p_reVal: usu2);
+                //   int valusu3 = valoracionusuariocen.New_ (p_puntuacion: 5, p_emVal: usu2, p_reVal: usu2);
+
+                //  Console.WriteLine ("// ---------------------------------------------------------------------------------------------------------------------------------//");
+                //   IList<ValoracionUsuarioEN> listavalusu = valoracionusuariocen.ReadAll (0, 3);
+                //   Console.WriteLine ("// ---------------------------------------------------------------------------------------------------------------------------------//\n");
+                //   Console.WriteLine ("MOSTRAMOS LPOS LINEAS DE DEVOLUCIONES");
+                //   foreach (ValoracionUsuarioEN p in listavalusu) {
+                //        Console.WriteLine (p.Puntuacion);
+                //   }
+                //   Console.WriteLine ("");
+                //   Console.WriteLine ("// ---------------------------------------------------------------------------------------------------------------------------------//");
+                //   Console.WriteLine ("");
+                //   valoracionusuariocen.Modify (valusu1, p_puntuacion: 1);
+                //   Console.WriteLine ("");
+                //   Console.WriteLine ("// ---------------------------------------------------------------------------------------------------------------------------------//");
+                //   Console.WriteLine ("");
+                //   ValoracionUsuarioEN valusuen = valoracionusuariocen.ReadOID (valusu1);
+                //   Console.WriteLine (""); Console.WriteLine ("");
+                //   Console.WriteLine (valusuen.Puntuacion);
+                //   Console.WriteLine ("");
+                //  Console.WriteLine ("// ---------------------------------------------------------------------------------------------------------------------------------//");
+                //   Console.WriteLine ("");
+                //   valoracionusuariocen.Destroy (valusu1);
+                //   listavalusu = valoracionusuariocen.ReadAll (0, 3);
+                //   Console.WriteLine ("// ---------------------------------------------------------------------------------------------------------------------------------//\n");
+                //   Console.WriteLine ("MOSTRAMOS LPOS LINEAS DE DEVOLUCIONES");
+                //   foreach (ValoracionUsuarioEN p in listavalusu) {
+                //       Console.WriteLine (p.Puntuacion);
+                //   }
+
+
+
+
+                //   //              int publicacionId6 = publicacioncen.New_(p_tipo: ArthookGen.ApplicationCore.Enumerated.Arthook.TipoProdEnum.video, p_descripcion: "esto es una video", p_titulo: "Video2", p_tipoPublicacion: ArthookGen.ApplicationCore.Enumerated.Arthook.TipoPublicacionEnum.Publicacion, p_usuarioPublicacion: usu3);
+                //   //              usuariocen.Modify(p_Usuario_OID: usu3, p_pass: "ter", p_nombre: "teresita", p_email: "tgcg2@alu.ua.es", p_nickname: "ter2610", p_tipoUsuario: ArthookGen.ApplicationCore.Enumerated.Arthook.TipoUsuarioEnum.usuario);
+                //   //              usuariocen.Destroy(usu1);
+
+
+                //   Console.WriteLine("");
+                //   Console.WriteLine("// ---------------------------------------------------------------------------------------------------------------------------------//");
+                //   Console.WriteLine("// ---------------------------------------------------------------------------------------------------------------------------------//");
+                //   Console.WriteLine("// ---------------------------------------------------------------------------------------------------------------------------------//");
+                //   Console.WriteLine("");
+
 
 
                 foreach (UsuarioEN user in listaUNombre) {
